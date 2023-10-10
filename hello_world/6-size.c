596cedd3606c2e985cd5655c29a547deac444fa1
root <eithanbtabarez14@gmail.com>
2023-10-10 15:45:42 -0700

    init

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-1
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+
+	return (0);
+}
+u
