18bc6dad4f2cd9e46288a99cd77e9134f23b1780
root <eithanbtabarez14@gmail.com>
2023-10-10 15:41:25 -0700

    init

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-1
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
