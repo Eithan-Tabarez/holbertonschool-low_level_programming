5522673c67373c10c9a6a4de757d1432cdc47c9f
root <eithanbtabarez14@gmail.com>
2023-10-11 05:51:23 -0700

    C

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if((n%10) >5)
+	{
+		printf("Last digit of\5"\n);
+	}
+	return (0);
+}
