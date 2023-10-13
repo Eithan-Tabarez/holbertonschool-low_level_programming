539c0370bae851058a84dcafbcea5955b9ed8ad2
root <eithanbtabarez14@gmail.com>
2023-10-13 09:51:07 -0700

    C

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
+ * print_diagonal - print a backslash.
+ * @n: n lines.
  *
- *
- *
+ * Return: 0 when n <= 0.
  *
  */
- 
 void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_print('\n)
-		return (0);
+		_putchar('\n');
+		return;
 	}
 	for (; n >= 1; n--)
 		_putchar(' ');
-	_putchar('\');
+	_putchar('\\');
 }
