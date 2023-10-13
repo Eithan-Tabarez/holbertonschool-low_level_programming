c927ffcfe38776d7d198e4a65e6bfc70840aa0af
root <eithanbtabarez14@gmail.com>
2023-10-13 09:35:19 -0700

    C

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+ 
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_print('\n)
+		return (0);
+	}
+	for (; n >= 1; n--)
+		_putchar(' ');
+	_putchar('\');
+}
