9290ba786c0da912e88afd90e8cafe528c079504
root <eithanbtabarez14@gmail.com>
2023-10-13 10:55:20 -0700

    C

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Print FizzBuzz
+ *
+ *
+ * Return: success
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		if (i == 100)
+			printf("\n");
+		else
+			printf(" ");
+	}
+	return (0);
+}
