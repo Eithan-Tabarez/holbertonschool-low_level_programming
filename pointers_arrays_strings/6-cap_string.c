cbb692ea4ab2be249dccc06e8910abc5eb87c7a8
root <eithanbtabarez14@gmail.com>
2023-10-19 07:28:59 -0700

    C

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,34 @@
 #include "main.h"
 /**
+* cap_string - capitalizes most of the words in a string.
+* @a: analized string.
 *
-*
-*
+* Return: String with all words capitalized.
 */
-char *cap_string(char *)
+char *cap_string(char *q)
+{
+	int i, j;
+	int a[] = {};
+
+	i  = 0;
+	while (*(q + i) != '\0')
+	{
+		if (*(q + i) >= 'a' && *(q + i) <= 'z')
+		{
+			if ( i == 0)
+			{
+				*(q + i) = *(q + i) - 32;
+			}
+			else
+			{
+				if (q[j] == *(q + i))
+				{
+					*(q + i) = *(q + i) - 32;
+				}
+			}
+		}
+	i++;
+	}
+	return (q);
+}
+
