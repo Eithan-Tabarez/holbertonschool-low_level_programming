b90f5f99c2d024550fcd227ea9490ddddd06a207
root <eithanbtabarez14@gmail.com>
2023-10-23 06:05:59 -0700

    C

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+* coincidence - define if string is b inside a.
+* @a: source string
+* @b: string to be search
+*
+* Return: 1 or 0
+*/
+int coincidence(char *a, char *b)
+{
+        while (*b && *b == *a)
+        {
+                b++;
+                a++;
+        }
+
+        if (*b == '\0')
+                return (1);
+        else
+                return (0);
+}
+
+/**
+* _strstr - prints the consecutive chars.
+* @haystack: source string.
+* @needle: searching a string.
+*
+* Return: new string.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		{
+			return (haystack);
+		}
+		else
+		{
+			haystack++;
+		}
+	}
+	return (0);
+}
