4728a4f03b3a5e7013d0e09841e1c59c5588e6c2
root <eithanbtabarez14@gmail.com>
2023-10-24 07:46:14 -0700

    C

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _sqrt_recursion - return sqrt of a number
+* @n: number.
+*
+* Return: sqrt 0 if not natural.
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return(-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (sqrt_m(n, 1));
+}
+
+/**
+* sqrt_m: calculate the sqrt
+* @n: number
+* @i: counter
+*
+* Return: sqrt
+*/
+int sqrt_m(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (sqrt_m(n, i + 1));
+}
