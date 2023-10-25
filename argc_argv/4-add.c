c4494cf001b3f7e890bfd7f3a0f93047ed433f6a
root <Eithanbtabarez14@gmail.com>
2023-10-25 09:46:39 -0700

    C

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	for (num = 1; num < argc; num++)
 	{
 		for (digit = 0; argv[num][digit]; digit++)
-		{	
+		{
 			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
 				printf("Error\n");
