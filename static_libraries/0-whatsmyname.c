05f0c8df008474038610f67af5a576d93644dd25
root <Eithanbtabarez14@gmail.com>
2023-10-25 06:50:11 -0700

    C

---
+++ static_libraries/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+/**
+ * main - print the program name 
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ *
+ * Return: alqays 0. 
+ */
+int main(int _attribute_((_unused_)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
