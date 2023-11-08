6e0e10dc9305c46697fd896f121ca208b8bec6da
root <Eithanbtabarez14@gmail.com>
2023-11-08 11:22:47 -0800

    C

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,11 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 
 	va_list nums;
-	unsigned int index, sum = 0;
+	unsigned int i, sum = 0;
 
 	va_start(nums, n);
 
-	for (index = 0; index < n; index++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
 
 	va_end(nums);
