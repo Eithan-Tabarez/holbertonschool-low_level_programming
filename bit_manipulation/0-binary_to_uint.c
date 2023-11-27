eb14dc5f77b9f608eea5082ab6325ecf8afc69d6
root <Eithanbtabarez14@gmail.com>
2023-11-27 10:50:27 -0800

    c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary string to decimal
- * @b: binary string
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return: the result
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
 
-unsigned int binary_to_uint(const char *b) {
-    if (*b == NULL)
-        return 0;
+	if (*b == '\0')
+		return (0);
 
-    unsigned int result = 0;
+	for (len = 0; b[len];)
+		len++;
 
-    while (*b != '\0') {
-        if (*b != '0' && *b != '1') {
-            return 0; 
-        }
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 
-        result = (result << 1) + (*b - '0');
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
 
-        b++;
-    }
-
-    return result;
+	return (num);
 }
