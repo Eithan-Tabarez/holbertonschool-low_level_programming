06372639569379ec08a685eb4f136e1689fdc1ea
root <EithanbTabarez14@gmail.com>
2024-02-27 14:03:13 +0000

    pointers arrays strings

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.c"
+
+/**
+ * puts2 - print one char out 2 of a string.
+ *
+ *  @str: string to print the string.
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	while (str[i] != '\0'
+	{
+		j++;
+	}
+
+	for (i =0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
