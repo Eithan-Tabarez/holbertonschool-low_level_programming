8aa9a598783046f4dd11556c5c322c269b3fce8c
root <EithanbTabarez14@gmail.com>
2024-02-28 14:37:41 +0000

    argc_argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print the program name.
+ * @argc: number of arguments.
+ * @argv: array of a pointers to the arguments.
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
