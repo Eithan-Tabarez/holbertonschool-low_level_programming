aeb73a2d4dc5a10beeb8bae10eb55ba88ad48665
root <EithanbTabarez14@gmail.com>
2024-03-06 12:46:01 +0000

    function pointer

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * array_iterator - executes a function as a parameter of an array
+ * @array: array}
+ * @size: size of array
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * int_index - Searches integers in a array.
+ * @array: The array.
+ * @size: The size of the array.
+ * @cmp: A pointer of the function.
+ *
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
