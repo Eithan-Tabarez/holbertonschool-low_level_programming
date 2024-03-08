570e7c7ca104996baf08474b21bcd06079168d69
Eithan-Tabarez <146757670+Eithan-Tabarez@users.noreply.github.com>
2024-03-08 13:14:05 -0300

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,56 +2,56 @@
 
 void print_all(const char *format, ...)
 {
-    va_list args;
-    const char *ptr;
-    char c;
-    int i;
-    float f;
-    char *s;
+	va_list args;
+	const char *ptr;
+	char c;
+	int i;
+	float f;
+	char *s;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (ptr = format; *ptr != '\0'; ptr++)
+	for (ptr = format; *ptr != '\0'; ptr++)
 	{
-        c = *ptr;
-        switch (c)
+		c = *ptr;
+		switch (c)
 		{
-            case 'c':
-                i = va_arg(args, int);
-                printf("%c", i);
-                break;
-            case 'i':
-                i = va_arg(args, int);
-                printf("%d", i);
-                break;
-            case 'f':
-                f = (float)va_arg(args, double);
-                printf("%f", f);
-                break;
-            case 's':
-                s = va_arg(args, char *);
-                if (s == NULL)
-                    printf("(nil)");
-                else
-                    printf("%s", s);
-                break;
-            default:
-                break;
-        }
-    }
+			case 'c':
+				i = va_arg(args, int);
+				printf("%c", i);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = (float)va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
+			default:
+				break;
+		}
+	}
 
-    va_end(args);
-    printf("\n");
+	va_end(args);
+	printf("\n");
 }
 
 int main()
 {
-    char c = 'A';
-    int i = 123;
-    float f = 3.14;
-    char *s = NULL;
+	char c = 'A';
+	int i = 123;
+	float f = 3.14;
+	char *s = NULL;
 
-    print_all("cifs", c, i, f, s);
+	print_all("cifs", c, i, f, s);
 
-    return 0;
+	return 0;
 }
