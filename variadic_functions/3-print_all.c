23c980d55b8c663754c161388e26c407307643ad
Eithan-Tabarez <146757670+Eithan-Tabarez@users.noreply.github.com>
2024-03-11 10:18:03 -0300

    variadic_functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -76,7 +76,7 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0, j = 0;
 	char *separator = "";
-	format_function_pair funcs[] =
+	printer_t funcs[] =
 	{
 		{"c", print_char},
 		{"i", print_int},
